913ec06c1b30a0cdcd1ab4f5a78cd6aacc14d4b4
LegendPhoenix66 <64265723+LegendPhoenix66@users.noreply.github.com>
2025-02-09 11:53:01 +0100

    norm

--- src/check_args.c
+++ src/check_args.c
@@ -6,7 +6,7 @@
 /*   By: lhopp <lhopp@student.42luxembourg.lu>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 11:32:27 by lhopp             #+#    #+#             */
-/*   Updated: 2025/02/09 11:32:27 by lhopp            ###   ########.fr       */
+/*   Updated: 2025/02/09 11:50:16 by lhopp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,28 @@
 
 void	check_args(int argc, char **argv)
 {
-    size_t	len;
-    char	*file_name;
+	size_t	len;
+	char	*file_name;
 
-    if (argc != 2)
-    {
-        printf("Error\nExpected 1 arguments, received %d\n", argc - 1);
-        exit(1);
-    }
-    file_name = ft_strrchr(argv[1], '/');
-    if (file_name)
-        file_name++;
-    else
-        file_name = argv[1];
-    len = ft_strlen(file_name);
-    if (len < 4 || ft_strncmp(file_name + len - 4, ".ber", 4))
-    {
-        printf("Error\nFile extension should be '.ber'\n");
-        exit(1);
-    }
-    else if (len == 4)
-    {
-        printf("Error\nFile name should not be empty\n");
-        exit(1);
-    }
+	if (argc != 2)
+	{
+		printf("Error\nExpected 1 arguments, received %d\n", argc - 1);
+		exit(1);
+	}
+	file_name = ft_strrchr(argv[1], '/');
+	if (file_name)
+		file_name++;
+	else
+		file_name = argv[1];
+	len = ft_strlen(file_name);
+	if (len < 4 || ft_strncmp(file_name + len - 4, ".ber", 4))
+	{
+		printf("Error\nFile extension should be '.ber'\n");
+		exit(1);
+	}
+	else if (len == 4)
+	{
+		printf("Error\nFile name should not be empty\n");
+		exit(1);
+	}
 }
--- src/main.c
+++ src/main.c
@@ -6,43 +6,41 @@
 /*   By: lhopp <lhopp@student.42luxembourg.lu>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 21:05:41 by lhopp             #+#    #+#             */
-/*   Updated: 2025/02/09 11:33:29 by lhopp            ###   ########.fr       */
+/*   Updated: 2025/02/09 11:51:04 by lhopp            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int cleanup(t_window *window){
-    if (window->win)
-        mlx_destroy_window(window->mlx, window->win);
-    if (window->mlx)
-    {
-        mlx_destroy_display(window->mlx);
-        free(window->mlx);
-    }
-    gc_clean();
-    return 0;
+int	cleanup(t_window *window)
+{
+	if (window->win)
+		mlx_destroy_window(window->mlx, window->win);
+	if (window->mlx)
+	{
+		mlx_destroy_display(window->mlx);
+		free(window->mlx);
+	}
+	gc_clean();
+	return (0);
 }
 
-int main(int argc, char **argv) {
+int	main(int argc, char **argv)
+{
+	t_window	window;
 
-    t_window window;
-
-    window.mlx = mlx_init();
-    window.win = NULL;
-    window.width = 1920;
-    window.height = 1080;
-
-    // check arguments, map, etc.
-    check_args(argc, argv);
-
-    window.win = mlx_new_window(window.mlx, window.width,
-                                window.height, "cub3d");
-
-    mlx_hook(window.win, DESTROY_NOTIFY, 0, &mlx_loop_end,
-             window.mlx);
-    mlx_key_hook(window.win, &esc_handler, window.mlx);
-    mlx_loop(window.mlx);
-    cleanup(&window);
-    return 0;
+	window.mlx = mlx_init();
+	window.win = NULL;
+	window.width = 1920;
+	window.height = 1080;
+	// check arguments, map, etc.
+	check_args(argc, argv);
+    // validate_map
+	window.win = mlx_new_window(window.mlx, window.width, window.height,
+			"cub3d");
+	mlx_hook(window.win, DESTROY_NOTIFY, 0, &mlx_loop_end, window.mlx);
+	mlx_key_hook(window.win, &esc_handler, window.mlx);
+	mlx_loop(window.mlx);
+	cleanup(&window);
+	return (0);
 }
